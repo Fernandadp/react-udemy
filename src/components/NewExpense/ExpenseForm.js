e4cc68268e0fab47b600d2597524217424b7f302
Fernanda Dehays <fernanda_dehays@hotmail.com>
2022-08-12 12:43:48 +0200

    use one state instead of three different (s4 - 55.)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,23 +3,38 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+
+    const [userInput, setUserInput] = useState({
+        setEnteredTitle: '',
+        setEnteredAmount: '',
+        setEnteredDate: ''
+    })
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        console.log(event.target.value);
+        //setEnteredTitle(event.target.title)
+        setUserInput({
+            ...userInput,
+            setEnteredTitle: event.target.value,
+        });
     };
 
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-        console.log(event.target.value);
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            setEnteredAmount: event.target.value,
+        });
     };
 
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        console.log(event.target.value);
+        // setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            setEnteredDate: event.target.value,
+        });
     };
 
 	return (
