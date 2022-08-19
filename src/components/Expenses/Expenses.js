3b9ea3f93b3c6ad266f013f0b73ac0bd98172893
Fernanda Dehays <fernanda_dehays@hotmail.com>
2022-08-19 12:52:51 +0200

    create a new component (expenses list) & check condition bfore return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../ui/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -15,19 +15,6 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
-	let expensesContent = <p>No expenses found.</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<div>
 			<Card className='expenses'>
@@ -35,32 +22,7 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{expensesContent}
-				{/* using ternary operator
-				{filteredExpenses.length === 0 ? (
-					<p>No expenses found.</p>
-				) : (
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))
-					)}*/}
-				{/* && approach, it checks the first part of the && statement and if it's true, it returns the second part
-				{filteredExpenses.length === 0 && <p>No expenses found.</p>}
-				{filteredExpenses.length > 0 &&
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))}
-				*/}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
