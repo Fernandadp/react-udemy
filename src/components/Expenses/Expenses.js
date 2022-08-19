98862cf52567c64bcbbe0329bf1a13d67de8f342
Fernanda Dehays <fernanda_dehays@hotmail.com>
2022-08-19 12:39:56 +0200

    output conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,30 +11,56 @@ const Expenses = (props) => {
 		setFilteredYear(selectedYear);
 	};
 
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
+	let expensesContent = <p>No expenses found.</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<div>
 			<Card className='expenses'>
-				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-				<ExpenseItem
-					title={props.items[0].title}
-					amount={props.items[0].amount}
-					date={props.items[0].date}
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
 				/>
-				<ExpenseItem
-					title={props.items[1].title}
-					amount={props.items[1].amount}
-					date={props.items[1].date}
-				/>
-				<ExpenseItem
-					title={props.items[2].title}
-					amount={props.items[2].amount}
-					date={props.items[2].date}
-				/>
-				<ExpenseItem
-					title={props.items[3].title}
-					amount={props.items[3].amount}
-					date={props.items[3].date}
-				/>
+				{expensesContent}
+				{/* using ternary operator
+				{filteredExpenses.length === 0 ? (
+					<p>No expenses found.</p>
+				) : (
+					filteredExpenses.map((expense) => (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					))
+					)}*/}
+				{/* && approach, it checks the first part of the && statement and if it's true, it returns the second part
+				{filteredExpenses.length === 0 && <p>No expenses found.</p>}
+				{filteredExpenses.length > 0 &&
+					filteredExpenses.map((expense) => (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					))}
+				*/}
 			</Card>
 		</div>
 	);
